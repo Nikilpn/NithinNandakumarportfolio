3c154042e464a5607be5a10635fee48436d019d9
Nikilpn <nikhilpnnick1234@gmail.com>
2026-07-24 16:12:23 +0530

    hi

--- portfolio_/portfolio/src/pages/Home.jsx
+++ portfolio_/portfolio/src/pages/Home.jsx
@@ -8,17 +8,25 @@ import Education from "../components/Education/Education";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 import PointCloud from "../components/Three/PointCloud";
+import CoordSystem from "../components/Three/CoordSystem";
+import GpsSurvey from "../components/Three/GpsSurvey";
 
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <About />
-      <div className="skills-experience-wrapper">
-        <PointCloud />
-        <Skills />
-        <Experience />
+      <div className="about-skills-wrapper">
+        <div className="about-bg-anim">
+          <CoordSystem />
+          <GpsSurvey />
+        </div>
+        <About />
+        <div className="skills-experience-wrapper">
+          <PointCloud />
+          <Skills />
+          <Experience />
+        </div>
       </div>
       <Projects />
       <Education />
